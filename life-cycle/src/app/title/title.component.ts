2b5564ba043ef89f953fe9b8b35887c13089d2fb
Rafael Dametto <cacotpds@gmail.com>
2023-10-04 14:57:17 -0300

    OnInit and OnChange hooks lifecycle

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css'],
 })
-export class TitleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() nome: string = '';
+  constructor() {
+    console.log(`Construtor ${this.nome}`);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`OnChanges ${this.nome}`);
+  }
+  ngOnInit(): void {
+    this.nome = this.nome + ' x';
+    console.log(`OnInit ${this.nome}`);
+  }
 }
